b0b5f3b081392665c07ee59904b47ccab925dbba
kanastrax76 <108521251+kanastrax76@users.noreply.github.com>
2022-08-19 11:55:49 +0300

    .

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 (async () => {
+
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
@@ -6,9 +7,4 @@
             coordsEl.parentElement.classList.remove('invisible');
         });
     }
-<<<<<<< HEAD
-})();
-=======
-    // TODO: add custom logic here
-})();
->>>>>>> 6c669e26030d5edcd81dd997b7be3c22d797b739
+})
